a371416521fcd2ac8ce4ee33892278a28ac375c3
Alexander <75215204+webexpensive@users.noreply.github.com>
2021-08-05 13:43:37 +0300

    Add files via upload

---
+++ src/MyProject/Exceptions/NotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MyProject\Exceptions;
+
+class NotFoundException extends \Exception
+{
+}
+
+?>
---
+++ src/MyProject/Exceptions/UnauthorizedException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MyProject\Exceptions;
+
+class UnauthorizedException extends \Exception
+{
+}
+
+?>
