f757092cc22738587a95722a7eaa3fcdc40ccd2f
Alexander <75215204+webexpensive@users.noreply.github.com>
2021-08-05 13:42:12 +0300

    Create AbstractController.php

---
+++ src/MyProject/Controllers/AbstractController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MyProject\Controllers;
+
+use MyProject\Models\Users\User;
+use MyProject\Models\Users\UsersAuthService;
+use MyProject\Models\Pages\FeaturesPageService;
+use MyProject\View\View;
+
+abstract class AbstractController
+{
+    /** @var View */
+    protected $view;
+
+    /** @var User|null */
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = UsersAuthService::getUserByToken();
+        $this->view = new View(__DIR__ . '/../../../templates');
+        $this->view->setVar('user', $this->user);
+		$this->view->setVar('features_page', FeaturesPageService::getFeaturesPage());
+    }
+}
+
+?>
