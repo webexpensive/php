c4a1426d287a7f718c26a3159f0ea18cecebd521
Alexander <75215204+webexpensive@users.noreply.github.com>
2021-08-05 14:07:24 +0300

    Update 404.php

--- templates/errors/404.php
+++ templates/errors/404.php
@@ -1,1 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Страница не найдена</title>
+	<meta name="description" content="Описание тестового блога">
+	<meta name="keywords" content="Ключевые, слова, тестового, блога">
+	<meta name="author" lang="ru" content="Александр">
+	<meta name="generator" content="WebExp 0.1">
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="/css/styles.css">
+<body>
 <h1>Страница не найдена</h1>
+</body>
+</html>
