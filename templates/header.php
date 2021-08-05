5cdd4d2092b3060b5b3f616137c7796b668c6395
Alexander <75215204+webexpensive@users.noreply.github.com>
2021-08-05 14:10:37 +0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,6 @@
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 	<link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
